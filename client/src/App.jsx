c9de015c782c019dbbe6787b2459893fce24f278
Harivansh <jazzz1815@gmail.com>
2025-04-20 21:54:15 +0530

    define routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import { useState } from "react";
 
+import { Routes, Route } from "react-router";
+
+import Navbar from "./components/Navbar";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingPage from "./pages/SettingPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
-
   return (
     <>
-  <h1 className='bg-amber-300'> vansh sharma</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/settings" element={<SettingPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
